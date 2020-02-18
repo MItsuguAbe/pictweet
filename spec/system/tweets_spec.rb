97eecae3908fa84b3b25a07e80d0782fdc56bccc
MItsuguAbe <kouiy42@gmail.com>
2020-02-19 00:53:54 +0900

    letの練習
    意味のないテストですが、ユーザーBでログインしてもユーザーAの投稿が見れることのテストをletを使って書いてみる。

--- spec/system/tweets_spec.rb
+++ spec/system/tweets_spec.rb
@@ -2,22 +2,33 @@ require 'rails_helper'
 
 describe 'ツイート機能', type: :system do
     describe '一覧表示機能' do
+        let(:user_a) {FactoryBot.create(:user, nickname: 'ユーザーA', email: 'a@example.com')}
+        let(:user_b) {FactoryBot.create(:user, nickname: 'ユーザーB', email: 'b@example.com')}
+
         before do
-            user_a=FactoryBot.create(:user, nickname: 'ユーザー', email: 'a@example.com')
             FactoryBot.create(:tweet, text: '最初の投稿',  user: user_a)
+            visit new_user_session_path
+            fill_in 'Email', with: 'a@example.com'
+            fill_in 'Password', with: 'password'
+            click_button 'Log in'
         end
 
         context 'ユーザーAがログインしている時' do
-            before do
-                visit new_user_session_path
-                fill_in 'Email', with: 'a@example.com'
-                fill_in 'Password', with: 'password'
-                click_button 'Log in'
-            end
+            let(:login_user){user_a}
 
-            it 'ユーザーAが作成したタスクが表示される' do
+            it 'ユーザーAが作成したツイートが表示される' do
                 expect(page).to have_content '最初の投稿'
             end
         end
+   
+        context 'ユーザーBがログインしている時' do
+            let(:login_user){user_b}
+
+            it 'ユーザーAが作成したツイートが表示される' do
+                expect(page).to have_content '最初の投稿'
+            end
+        end
+   
+   
     end
 end
