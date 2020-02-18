5f2cdfe744d294865abbd6559cd97be6f8e06bde
MItsuguAbe <kouiy42@gmail.com>
2020-02-19 01:19:14 +0900

    詳細表示機能のspecの追加

--- spec/system/tweets_spec.rb
+++ spec/system/tweets_spec.rb
@@ -1,18 +1,19 @@
 require 'rails_helper'
 
 describe 'ツイート機能', type: :system do
+    let(:user_a) {FactoryBot.create(:user, nickname: 'ユーザーA', email: 'a@example.com')}
+    let(:user_b) {FactoryBot.create(:user, nickname: 'ユーザーB', email: 'b@example.com')}
+    let!(:tweet_a) {FactoryBot.create(:tweet, text: '最初の投稿', user: user_a)}
+
+
+    before do
+        visit new_user_session_path
+        fill_in 'Email', with: login_user.email
+        fill_in 'Password', with: login_user.password
+        click_button 'Log in'
+    end
+
     describe '一覧表示機能' do
-        let(:user_a) {FactoryBot.create(:user, nickname: 'ユーザーA', email: 'a@example.com')}
-        let(:user_b) {FactoryBot.create(:user, nickname: 'ユーザーB', email: 'b@example.com')}
-
-        before do
-            FactoryBot.create(:tweet, text: '最初の投稿',  user: user_a)
-            visit new_user_session_path
-            fill_in 'Email', with: 'a@example.com'
-            fill_in 'Password', with: 'password'
-            click_button 'Log in'
-        end
-
         context 'ユーザーAがログインしている時' do
             let(:login_user){user_a}
 
@@ -28,7 +29,20 @@ describe 'ツイート機能', type: :system do
                 expect(page).to have_content '最初の投稿'
             end
         end
-   
-   
     end
+
+    describe '詳細表示機能' do
+        context 'ユーザーAがログインしている時' do
+            let(:login_user){user_a}
+
+            before do 
+                visit tweet_path(tweet_a)
+            end
+
+            it 'ユーザーAが作成したツイートが表示される' do
+                expect(page).to have_content '最初の投稿'
+            end
+        end
+    end
+
 end
