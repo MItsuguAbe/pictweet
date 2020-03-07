ba91a37f3ed7cfaf6bd767175f9cfc65eb7c29ad
MItsuguAbe <kouiy42@gmail.com>
2020-03-07 16:11:15 +0900

    respond_to
    respond_toでHTMLとJSONの条件分岐

--- app/controllers/tweets/searches_controller.rb
+++ app/controllers/tweets/searches_controller.rb
@@ -1,5 +1,11 @@
 class Tweets::SearchesController < ApplicationController
     def index
         @tweets = Tweet.search(params[:keyword])   #searchメソッドはtweetモデルに定義されてます
+        respond_to do |format|
+            format.html
+            format.json
+        end
     end
 end
+
+
