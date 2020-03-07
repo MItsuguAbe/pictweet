9aeaff4b57e40067d4450879d83e9609edc931ec
MItsuguAbe <kouiy42@gmail.com>
2020-03-07 14:50:40 +0900

    respond_toでHTMLとJSONを分岐
    commentコントローラーでrespond_toを使い、HTMLとJSONを分岐させる。

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,12 @@
 class CommentsController < ApplicationController
     def create
-        comment=Comment.create(comment_params)
-        redirect_to "/tweets/#{comment.tweet.id}"  # コメントと結びつくツイートの詳細画面に遷移する
-      end
+        @comment = Comment.create(text: comment_params[:text], tweet_id: comment_params[:tweet_id], user_id: current_user.id)
+        respond_to do |format|
+            format.html { redirect_to tweet_path(params[:tweet_id])  }
+            format.json
+        end
+    end
+    #ローカル変数commentだとスコープの関係でこの後のjbuilder側で使用できないので、インスタンス変数@commentに編集します。
     
     private
     def comment_params
