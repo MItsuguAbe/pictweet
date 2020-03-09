92987a13c8d6d1837ee821a73c08038743eee1a5
MItsuguAbe <kouiy42@gmail.com>
2020-03-09 22:02:04 +0900

    システムスペックの続き
    コメント機能、検索機能、いいね機能のテストを追加。

--- spec/system/tweets_spec.rb
+++ spec/system/tweets_spec.rb
@@ -45,7 +45,7 @@ describe 'ツイート機能', type: :system do
             visit new_tweet_path
             fill_in 'text', with: tweet_text
             find(".form-group")
-            #find(".form-check-label") #なんでこっちはみつからないのかと思ったら、input type="hidden"になってる。。なぜ？
+            #find(".form-check", visible: false) #なんでこっちはみつからないのかと思ったら、input type="hidden"になってる。。なぜ？
             click_button 'SEND'
         end
 
@@ -53,6 +53,49 @@ describe 'ツイート機能', type: :system do
             let(:tweet_text){'新規作成のテストを書く'}
             it '投稿成功' do
                 expect(page).to have_content '投稿が完了しました。'
+            end
+        end
+    end
+
+    describe 'コメント機能' do
+        context 'ユーザーAがログインしている時' do
+            let(:login_user){user_a}
+            before do 
+              visit tweet_path(tweet_a)
+              fill_in 'コメントする', with: "コメント機能のテスト"
+              click_button 'SEND'
+            end
+
+            it 'ユーザーAが作成したコメントが表示される' do
+                expect(page).to have_content "コメント機能のテスト"
+            end
+        end
+    end
+
+    describe '検索機能' do
+        context 'ユーザーAがログインしている時' do
+            let(:login_user){user_a}
+            before do 
+              fill_in '投稿を検索する', with: "最初の投稿"
+              click_button '検索'
+            end
+
+            it '最初の投稿の記事が表示される' do
+                expect(page).to have_content "最初の投稿"
+            end
+        end
+    end
+
+    describe 'いいね機能' do
+        context 'ユーザーAがログインしている時' do
+            let(:login_user){user_a}
+            before do 
+              visit tweet_path(tweet_a)
+              find('#like').click
+            end
+
+            it 'いいねの数が0から1になる' do
+                expect(page).to have_content "1"
             end
         end
     end
